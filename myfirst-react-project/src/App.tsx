135cf9e7fe5695d7ab42a11dfb2d559382fc81a7
sara <sara.ekmanMedieinstitutet@gmail.com>
2022-03-15 01:21:53 +0100

    added addition component

--- myfirst-react-project/src/App.tsx
+++ myfirst-react-project/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import { Addition } from './components/addition/Addition';
 import { HelloWorld } from './components/hello-world/HelloWorld';
 
 function App() {
   return (
     <div>
-    <HelloWorld name='Sara'age={22}/>
+      <HelloWorld name='Sara' age={22} />
+      <Addition/>
     </div>
   );
 }
---
+++ myfirst-react-project/src/components/addition/Addition.tsx
@@ -0,0 +1,17 @@
+import "./Addition.css";
+export function Addition() {
+    let x = 17;
+    let y = '4';
+
+    function A(){}
+    function B(){}
+
+
+    return (<div className="container">
+        <input type="text" />
+        <p>+</p>
+        <input type="text" />
+        <button>Spra</button>
+        <p>Result: </p>
+    </div>);
+}
